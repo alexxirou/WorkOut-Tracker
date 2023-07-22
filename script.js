8b335f36f52169491884b153c5910dee16818d26
Alexandros Xirouchakis <alex.xirou@hotmail.com>
2023-07-22 23:16:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -237,30 +237,26 @@ class App{
           <span class="workout__value">${workout.duration}</span>
           <span class="workout__unit">min</span>
         </div>
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.calcPace().toFixed(1)}</span>
+          <span class="workout__unit">min/km</span>
+        </div>
     `;
 
     if (workout.type === 'running')
       html += `
         <div class="workout__details">
-          <span class="workout__icon">⚡️</span>
-          <span class="workout__value">${workout.pace.toFixed(1)}</span>
-          <span class="workout__unit">min/km</span>
-        </div>
-        <div class="workout__details">
           <span class="workout__icon">🦶🏼</span>
           <span class="workout__value">${workout.cadence}</span>
           <span class="workout__unit">spm</span>
         </div>
+        
       </li>
       `;
 
     if (workout.type === 'cycling')
       html += `
-        <div class="workout__details">
-          <span class="workout__icon">⚡️</span>
-          <span class="workout__value">${workout.speed.toFixed(1)}</span>
-          <span class="workout__unit">km/h</span>
-        </div>
         <div class="workout__details">
           <span class="workout__icon">⛰</span>
           <span class="workout__value">${workout.elevationGain}</span>
